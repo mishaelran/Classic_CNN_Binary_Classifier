5a411a77194087353275f3baad010c868e3886cd
mishaelran <45035487+mishaelran@users.noreply.github.com>
2019-02-07 16:39:05 +0200

    Add files via upload

---
+++ selfBUilt CNN for classification.py
@@ -0,0 +1,92 @@
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.callbacks import ModelCheckpoint
+
+# Initialising the CNN
+classifier = Sequential()
+input_size = 256
+batch = 64
+# Step 1 - Convolution
+classifier.add(Conv2D(32, (3, 3), input_shape = (256, 256, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+#2nd layer CNN
+classifier.add(Conv2D(64, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+#3rd layer
+classifier.add(Conv2D(64, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+classifier.add(Conv2D(64, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+classifier.add(Conv2D(128, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+"""
+classifier.add(Conv2D(256, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+"""
+# Step 3 - Flattening
+classifier.add(Flatten())
+
+# Step 4 - Full connection
+classifier.add(Dropout(rate = 0.6))
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dropout(rate = 0.5))
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dropout(rate = 0.2))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics= ['accuracy'])
+# Part 2 - Fitting the CNN to the images
+
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('../dataset/training_set',
+                                                 target_size = (256, 256),
+                                                 batch_size = 64,
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('../dataset/test_set',
+                                            target_size = (256, 256),
+                                            batch_size = 64,
+                                            class_mode = 'binary')
+
+checkpointer = ModelCheckpoint(filepath='/tmp/weights.hdf5', verbose=1, save_best_only=True)
+
+classifier.fit_generator(training_set,
+                         max_queue_size=10,
+                         steps_per_epoch = 8000/64,
+                         epochs = 25,
+                         validation_data = test_set,
+                         validation_steps = 2000/64)
+
+# Part 3 - evaluate new images prediction. 
+"""
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
+"""
